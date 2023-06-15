3cd29a0170b7434094e41b0a50daba843f2e3732
rnpacifique <rpacifique12@gmail.com>
2023-06-15 16:31:05 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entryy point
- * return : 0
+ * return : 0 (success)
  */
-int main (void)
-
+int main(void)
 {    
-	puts("\"Programming is like building a multilingual puzzle\n");
-	 return (0);
-
-}	 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
