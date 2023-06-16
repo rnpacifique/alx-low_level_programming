76dae7972a418bc4138d952cfb4037981ebbc7d4
rnpacifique <rpacifique12@gmail.com>
2023-06-16 01:55:41 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entryy point
- * return : 0 (success)
+ * return : 0
  */
 int main(void)
-{    
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main Entry point
+ * main - Entry point
  *
  * Return: 0
  */
@@ -9,5 +9,5 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-	
+
 
