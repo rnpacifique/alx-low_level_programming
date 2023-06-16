7e1da487be8ab0e76e18a6ca95e97c167225d3bc
rnpacifique <rpacifique12@gmail.com>
2023-06-16 03:52:53 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *a program that print a standard error
+ * return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
+
+
+
