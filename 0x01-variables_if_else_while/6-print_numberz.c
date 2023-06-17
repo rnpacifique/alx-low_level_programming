b4951f2b395d304f650f2cd192b6dedf755bbd13
rnpacifique <rpacifique12@gmail.com>
2023-06-16 17:06:01 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *  prints all single digit numbers of base 10 starting from 0
+ *  Return: 0
+ */
+int main(void)
+{
+	int digt = 0;
+
+	while (digt <= 9)
+
+	{
+		putchar(digt + '0');
+		++digt;
+	}
+	putchar('\n');
+	return (0);
+   }
