7b460f4e5041648e3a5eaabe4407cd4337dd989c
rnpacifique <rpacifique12@gmail.com>
2023-06-19 14:30:06 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,16 +5,18 @@
  * Return: 0
  */
 int main(void)
- {
-     int n;
-     int a;
+{
+	int n;
+	int a;
 
-     for (n =48; n <= 57; n++){
-	     putchar(n);
-     }
-     for (a =97; a <=102; a++){
-	     putchar(a);
-     }
-     putchar('\n');
-     return (0);
- }
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
