fac1d3d0fd6bc25efdf1ff376710f1e17e7020dd
rnpacifique <rpacifique12@gmail.com>
2023-06-20 15:26:52 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ * return: 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+}
+
